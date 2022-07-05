f1f89347123040dd67717a97181f4504320a6c41
Aleksey Semukov <semykov@mail.ru>
2022-07-05 20:56:31 +0300

    Дописал функцию поиска отелей и фотографий. Добавлен вывод информации по отелям пользователю (без сортировки)

--- t_bot/command/lowprice.py
+++ t_bot/command/lowprice.py
@@ -29,9 +29,8 @@ def get_lowprice_hotel(user_id):
     hotel_list = get_hotels_list(user_dict[user_id].get_querystring_lowprice())
     for hotel in hotel_list.values():
         bot.send_message(user_id, f"""
-        <b>Название отеля:</b> {hotel['name hotel']}        
+        <b>Название отеля:</b> {hotel['name hotel']} {hotel['starRating']}        
         <b>Адрес:</b> {hotel['address']}
-        <b>Звёздность отеля:</b> {hotel['starRating']}
         <b>Рейтинг отеля:</b> {hotel['unformattedRating']}"
         <b>Страницу с отелем:</b> {hotel['site']}"
         <b>Расположение от центра:</b> {hotel['landmarks']}
@@ -39,14 +38,3 @@ def get_lowprice_hotel(user_id):
       """)
         media_group = [types.InputMediaPhoto(media=url) for url in hotel['photo']]
         bot.send_media_group(user_id, media_group)
-
-
-
-# bot.send_media_group(message.chat.id, media_group)
-# bot = telegram.Bot(token = TOKEN)
-# media_group = []
-# text = 'some caption for album'
-# for num in range(3):
-#     media_group.append(InputMediaPhoto(open('img%d.png' % num, 'rb'),
-#                                        caption = text if num == 0 else ''))
-# bot.send_media_group(chat_id = CHANNEL_ID, media = media_group)
