6142b9a681430fe9823f5a11944c859543f4799b
Alex <semykov@mail.ru>
2022-07-15 14:05:30 +0300

    Добавлена проверка на пустую историю запросов

--- t_bot/command/history.py
+++ t_bot/command/history.py
@@ -1,24 +1,31 @@
 from config import bot
 from loguru import logger
-from database.users import User
 from database.db_func import get_request_db
 from t_bot.utilities import func
+from t_bot.keyboard_markup.inline_keyboard import after_search
 
 
 @logger.catch()
 @bot.message_handler(commands=['history'])
 def send_history(message):
-
     history = get_request_db(message.chat.id)
-    for i_hist in history:
-        message_s = f"""
+    answer_button = after_search()
+    if history:
+        for i_hist in history:
+            message_s = f"""
 <b>дата запроса:</b>  {i_hist['data'].strftime("%Y.%m.%d <b>время:</b>  %H:%M")}
 <b>Команда:</b> {i_hist['command'][1:]}
 <b>Запрашиваемый город:</b> {i_hist['city']}
 <b>Список найденных отелей:</b>"""
-        bot.send_message(message.chat.id, message_s)
-        for hotel in i_hist['hotel_list'].values():
-            bot.send_message(message.chat.id, func.format_message_for_user_history(hotel), disable_web_page_preview=True)
+            bot.send_message(message.chat.id, message_s)
+            for hotel in i_hist['hotel_list'].values():
+                bot.send_message(message.chat.id,
+                                 func.format_message_for_user_history(hotel),
+                                 disable_web_page_preview=True)
+            bot.send_message(message.chat.id, 'Начать новый поиск?',
+                             reply_markup=answer_button)
+    else:
+        bot.send_message(message.chat.id, 'Ваша история запросов пуста', reply_markup=answer_button)
 
 
 @logger.catch()
